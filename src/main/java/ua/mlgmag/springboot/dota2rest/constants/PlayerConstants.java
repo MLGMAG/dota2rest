1b3b252edc340c0b885efc7e78431a87a483d7e4
Magomed Akhmedov <akhmedovmagomed2001@gmail.com>
2018-06-28 13:35:47 +0300

    Added player constants (2)

---
+++ src/main/java/ua/mlgmag/springboot/dota2rest/constants/PlayerConstants.java
@@ -0,0 +1,9 @@
+package ua.mlgmag.springboot.dota2rest.constants;
+
+public class PlayerConstants {
+
+    public final static Long ZERO = 76561197960265728L;
+
+    public final static String PLAYER_PROFILE_PREFIX = "http://steamcommunity.com/profiles/";
+
+}
