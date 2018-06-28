52146c7caf95e05efd060883d9e294867bbab37f
Magomed Akhmedov <akhmedovmagomed2001@gmail.com>
2018-06-28 14:06:23 +0300

    Remove PlayerProfile model Field from PlayerProf moved to Player model

--- src/main/java/ua/mlgmag/springboot/dota2rest/model/Player.java
+++ src/main/java/ua/mlgmag/springboot/dota2rest/model/Player.java
@@ -3,12 +3,25 @@ package ua.mlgmag.springboot.dota2rest.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "players")
 public class Player {
-    private PlayerProfile playerProfile;
+
+    @Id
+    private int steamId32;
+    private String personaName;
+    private String name;
+    @Indexed(unique = true)
+    private String steamId64;
+    private String avatar;
+    private String steamUrl;
     private String solo_competitive_rank;
     private String competitive_rank;
+
 }
