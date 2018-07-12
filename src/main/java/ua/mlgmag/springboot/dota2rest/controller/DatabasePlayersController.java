0bad0b6d1507b5cb90416787f834247c65198123
Magomed Akhmedov <akhmedovmagomed2001@gmail.com>
2018-07-13 00:30:07 +0300

    RestController -> Controller

--- src/main/java/ua/mlgmag/springboot/dota2rest/controller/DatabasePlayersController.java
+++ src/main/java/ua/mlgmag/springboot/dota2rest/controller/DatabasePlayersController.java
@@ -1,16 +1,20 @@
 package ua.mlgmag.springboot.dota2rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ua.mlgmag.springboot.dota2rest.constants.UrlMappingConstants;
 import ua.mlgmag.springboot.dota2rest.definition.PlayerService;
 import ua.mlgmag.springboot.dota2rest.model.Player;
 import ua.mlgmag.springboot.dota2rest.services.ApiService;
 
-import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping(UrlMappingConstants.DATABASE_PLAYERS_CONTROLLER_REQUEST_MAPPING)
 public class DatabasePlayersController {
 
@@ -25,36 +29,53 @@ public class DatabasePlayersController {
     }
 
     @GetMapping
-    public List<Player> players() {
-        return playerService.findAll();
+    public String players(@RequestParam(value = "saveError", required = false) String saveError,
+                          @RequestParam(value = "deleteError", required = false) String deleteError,
+                          Model model) {
+        model.addAttribute("players", playerService.findAll());
+        model.addAttribute(new Player());
+        model.addAttribute("deleteError", deleteError != null);
+        model.addAttribute("saveError", saveError != null);
+        model.addAttribute("title", "Players");
+        return "Player/players";
     }
 
     @GetMapping("/{id}")
-    public Player player(@PathVariable("id") Integer id) {
-        return playerService.findById(id).orElse(null);
+    public String player(@PathVariable("id") Integer id, Model model) {
+        Player player = playerService.findById(id).orElse(null);
+
+        if (player == null) {
+            model.addAttribute("title", "Object not found");
+            return "player";
+        }
+
+        player.setIsInDB(playerService.existById(player.getSteamId32()));
+        model.addAttribute("player", player);
+        model.addAttribute("title", player.getName());
+        return "Player/player";
     }
 
     @GetMapping("/save")
-    public Player savePlayer(@RequestParam(value = "id") Integer id) {
+    public String savePlayer(@RequestParam(value = "id") Integer id) {
         Optional<Player> playerOptional = apiService.findPlayerById(id);
 
         if (!playerOptional.isPresent()) {
-            return null;
+            return UrlMappingConstants.REDIRECT + UrlMappingConstants.DATABASE_PLAYERS_CONTROLLER_REQUEST_MAPPING + "?saveError";
         }
 
         playerService.save(playerOptional.get());
-        return playerOptional.get();
+        return UrlMappingConstants.REDIRECT + UrlMappingConstants.DATABASE_PLAYERS_CONTROLLER_REQUEST_MAPPING;
     }
 
     @GetMapping("/delete")
-    public Player deletePlayer(@RequestParam("id") Integer id) {
+    public String deletePlayer(@RequestParam("id") Integer id) {
         Optional<Player> player = playerService.findById(id);
 
         if (!player.isPresent()) {
-            return null;
+            return UrlMappingConstants.REDIRECT + UrlMappingConstants.DATABASE_PLAYERS_CONTROLLER_REQUEST_MAPPING + "?deleteError";
         }
 
         playerService.delete(player.get());
-        return player.get();
+        return UrlMappingConstants.REDIRECT + UrlMappingConstants.DATABASE_PLAYERS_CONTROLLER_REQUEST_MAPPING;
     }
 }
