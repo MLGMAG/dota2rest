865d6511fd25246ca847917d5c94e55677489d3c
Magomed Akhmedov <akhmedovmagomed2001@gmail.com>
2018-07-13 01:09:05 +0300

    Added user controller

--- src/main/java/ua/mlgmag/springboot/dota2rest/constants/UrlMappingConstants.java
+++ src/main/java/ua/mlgmag/springboot/dota2rest/constants/UrlMappingConstants.java
@@ -5,4 +5,7 @@ public class UrlMappingConstants {
     public final static String REDIRECT = "redirect:/";
 
     public final static String DATABASE_PLAYERS_CONTROLLER_REQUEST_MAPPING = "database/players";
+
+    public final static String USER_CONTROLLER_REQUEST_MAPPING = "user";
+
 }
---
+++ src/main/java/ua/mlgmag/springboot/dota2rest/controller/UserController.java
@@ -0,0 +1,36 @@
+package ua.mlgmag.springboot.dota2rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.mlgmag.springboot.dota2rest.constants.UrlMappingConstants;
+import ua.mlgmag.springboot.dota2rest.model.User;
+import ua.mlgmag.springboot.dota2rest.services.SecurityServiceImpl;
+import ua.mlgmag.springboot.dota2rest.services.User.UserServiceImpl;
+
+@Controller
+@RequestMapping(UrlMappingConstants.USER_CONTROLLER_REQUEST_MAPPING)
+public class UserController {
+
+    private final UserServiceImpl userService;
+
+    private final SecurityServiceImpl securityService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService, SecurityServiceImpl securityService) {
+        this.userService = userService;
+        this.securityService = securityService;
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        User user = userService.findByUsername(securityService.findLoggedInUsername());
+        model.addAttribute("title", user.getUsername());
+        model.addAttribute("players", user.getPlayerCollection());
+        return "User/Profile";
+    }
+
+
+}
