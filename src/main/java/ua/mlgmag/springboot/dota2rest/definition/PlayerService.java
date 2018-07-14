edb95d19eb319da4f0b505e312207df2164b101f
Magomed Akhmedov <akhmedovmagomed2001@gmail.com>
2018-07-14 23:10:46 +0300

    Reformat code

--- src/main/java/ua/mlgmag/springboot/dota2rest/definition/PlayerService.java
+++ src/main/java/ua/mlgmag/springboot/dota2rest/definition/PlayerService.java
@@ -3,5 +3,7 @@ package ua.mlgmag.springboot.dota2rest.definition;
 import ua.mlgmag.springboot.dota2rest.model.Player;
 
 public interface PlayerService extends GenericService<Player, Integer> {
+
     Boolean existById(Integer id);
+
 }
