452b695ce960ff2f578f89dc7f0222b3b87ee349
Magomed Akhmedov <akhmedovmagomed2001@gmail.com>
2018-07-14 23:33:57 +0300

    Database config

--- src/main/java/ua/mlgmag/springboot/dota2rest/model/User.java
+++ src/main/java/ua/mlgmag/springboot/dota2rest/model/User.java
@@ -39,6 +39,7 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     @ElementCollection(targetClass = Authority.class, fetch = FetchType.EAGER)
     @JoinTable(name = "user_authority", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "authority", columnDefinition = "VARCHAR(255)", nullable = false)
     private Set<Authority> authorities;
 
     @Column(name = "non_expired", columnDefinition = "boolean", nullable = false)
